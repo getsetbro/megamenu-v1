2508c5936700bc48772ebe8d7726ec8afb06812b
Seth Broweleit <setbro@gmail.com>
2019-08-15 14:46:51 -0400

    page

---
+++ page/js.js
@@ -0,0 +1,227 @@
+var megamenu = {
+  Overall: [
+    { text: "Overall 01", url: "#oa01" },
+    { text: "Overall 02", url: "#oa02" },
+    { text: "Overall 03", url: "#oa03" },
+    { text: "Overall 04", url: "#oa04" },
+    { text: "Overall 05", url: "#oa05" },
+    { text: "Overall 06", url: "#oa06" },
+    { text: "Overall 07", url: "#oa07" },
+    { text: "Overall 08", url: "#oa08" },
+    { text: "Overall 09", url: "#oa09" },
+    { text: "Overall 10", url: "#oa10" },
+    { text: "Overall 11", url: "#oa11" },
+    { text: "Overall 12", url: "#oa12" },
+    { text: "Overall 13", url: "#oa13" },
+    { text: "Overall 14", url: "#oa14" },
+    { text: "Overall 15", url: "#oa15" },
+    { text: "Overall 16", url: "#oa16" },
+    { text: "Overall 17", url: "#oa17" },
+    { text: "Overall 18", url: "#oa18" },
+    { text: "Overall 19", url: "#oa19" },
+    { text: "Overall 20", url: "#oa20" },
+    { text: "Overall 21", url: "#oa21" },
+    { text: "Overall 22", url: "#oa22" },
+    { text: "Overall 23", url: "#oa23" }
+  ],
+  "Safety & Quality": [
+    { text: "Safety & Quality 01", url: "#sq01" },
+    { text: "Safety & Quality 02", url: "#sq02" },
+    { text: "Safety & Quality 03", url: "#sq03" },
+    { text: "Safety & Quality 04", url: "#sq04" },
+    { text: "Safety & Quality 05", url: "#sq05" },
+    { text: "Safety & Quality 06", url: "#sq06" },
+    { text: "Safety & Quality 07", url: "#sq07" },
+    { text: "Safety & Quality 08", url: "#sq08" },
+    { text: "Safety & Quality 09", url: "#sq09" },
+    { text: "Safety & Quality 10", url: "#sq10" },
+    { text: "Safety & Quality 11", url: "#sq11" },
+    { text: "Safety & Quality 12", url: "#sq12" },
+    { text: "Safety & Quality 13", url: "#sq13" },
+    { text: "Safety & Quality 14", url: "#sq14" },
+    { text: "Safety & Quality 15", url: "#sq15" }
+  ],
+  "Patient Experience": [
+    { text: "Patient Experience 01", url: "#pe01" },
+    { text: "Patient Experience 02", url: "#pe02" },
+    { text: "Patient Experience 03", url: "#pe03" },
+    { text: "Patient Experience 04", url: "#pe04" },
+    { text: "Patient Experience 05", url: "#pe05" },
+    { text: "Patient Experience 06", url: "#pe06" },
+    { text: "Patient Experience 07", url: "#pe07" },
+    { text: "Patient Experience 08", url: "#pe08" },
+    { text: "Patient Experience 09", url: "#pe09" },
+    { text: "Patient Experience 10", url: "#pe10" },
+    { text: "Patient Experience 11", url: "#pe11" },
+    { text: "Patient Experience 12", url: "#pe12" },
+    { text: "Patient Experience 13", url: "#pe13" },
+    { text: "Patient Experience 14", url: "#pe14" },
+    { text: "Patient Experience 15", url: "#pe15" },
+    { text: "Patient Experience 16", url: "#pe16" },
+    { text: "Patient Experience 17", url: "#pe17" },
+    { text: "Patient Experience 18", url: "#pe18" },
+    { text: "Patient Experience 19", url: "#pe19" },
+    { text: "Patient Experience 20", url: "#pe20" },
+    { text: "Patient Experience 21", url: "#pe21" },
+    { text: "Patient Experience 22", url: "#pe22" },
+    { text: "Patient Experience 23", url: "#pe23" },
+    { text: "Patient Experience 24", url: "#pe24" },
+    { text: "Patient Experience 25", url: "#pe25" },
+    { text: "Patient Experience 26", url: "#pe26" },
+    { text: "Patient Experience 27", url: "#pe27" },
+    { text: "Patient Experience 28", url: "#pe28" },
+    { text: "Patient Experience 29", url: "#pe29" }
+  ],
+  "Coordinated Care": [
+    { text: "Coordinated Care 01", url: "#cc01" },
+    { text: "Coordinated Care 02", url: "#cc02" },
+    { text: "Coordinated Care 03", url: "#cc03" },
+    { text: "Coordinated Care 04", url: "#cc04" },
+    { text: "Coordinated Care 05", url: "#cc05" },
+    { text: "Coordinated Care 06", url: "#cc06" },
+    { text: "Coordinated Care 07", url: "#cc07" },
+    { text: "Coordinated Care 08", url: "#cc08" },
+    { text: "Coordinated Care 09", url: "#cc09" },
+    { text: "Coordinated Care 10", url: "#cc10" },
+    { text: "Coordinated Care 11", url: "#cc11" },
+    { text: "Coordinated Care 12", url: "#cc12" },
+    { text: "Coordinated Care 13", url: "#cc13" },
+    { text: "Coordinated Care 14", url: "#cc14" },
+    { text: "Coordinated Care 15", url: "#cc15" },
+    { text: "Coordinated Care 16", url: "#cc16" },
+    { text: "Coordinated Care 17", url: "#cc17" },
+    { text: "Coordinated Care 18", url: "#cc18" },
+    { text: "Coordinated Care 19", url: "#cc19" },
+    { text: "Coordinated Care 20", url: "#cc20" },
+    { text: "Coordinated Care 21", url: "#cc21" },
+    { text: "Coordinated Care 22", url: "#cc22" },
+    { text: "Coordinated Care 23", url: "#cc23" },
+    { text: "Coordinated Care 24", url: "#cc24" },
+    { text: "Coordinated Care 25", url: "#cc25" },
+    { text: "Coordinated Care 26", url: "#cc26" },
+    { text: "Coordinated Care 27", url: "#cc27" },
+    { text: "Coordinated Care 28", url: "#cc28" },
+    { text: "Coordinated Care 29", url: "#cc29" },
+    { text: "Coordinated Care 30", url: "#cc30" },
+    { text: "Coordinated Care 31", url: "#cc31" }
+  ],
+  "Enterprise Population Health": [
+    { text: "Enterprise Population Health 01", url: "#eph01" },
+    { text: "Enterprise Population Health 02", url: "#eph02" },
+    { text: "Enterprise Population Health 03", url: "#eph03" },
+    { text: "Enterprise Population Health 04", url: "#eph04" },
+    { text: "Enterprise Population Health 05", url: "#eph05" },
+    { text: "Enterprise Population Health 06", url: "#eph06" },
+    { text: "Enterprise Population Health 07", url: "#eph07" },
+    { text: "Enterprise Population Health 08", url: "#eph08" },
+    { text: "Enterprise Population Health 09", url: "#eph09" },
+    { text: "Enterprise Population Health 10", url: "#eph10" },
+    { text: "Enterprise Population Health 11", url: "#eph11" },
+    { text: "Enterprise Population Health 12", url: "#eph12" },
+    { text: "Enterprise Population Health 13", url: "#eph13" },
+    { text: "Enterprise Population Health 14", url: "#eph14" },
+    { text: "Enterprise Population Health 15", url: "#eph15" },
+    { text: "Enterprise Population Health 16", url: "#eph16" },
+    { text: "Enterprise Population Health 17", url: "#eph17" },
+    { text: "Enterprise Population Health 18", url: "#eph18" },
+    { text: "Enterprise Population Health 19", url: "#eph19" },
+    { text: "Enterprise Population Health 20", url: "#eph20" },
+    { text: "Enterprise Population Health 21", url: "#eph21" },
+    { text: "Enterprise Population Health 22", url: "#eph22" },
+    { text: "Enterprise Population Health 23", url: "#eph23" },
+    { text: "Enterprise Population Health 24", url: "#eph24" },
+    { text: "Enterprise Population Health 25", url: "#eph25" },
+    { text: "Enterprise Population Health 26", url: "#eph26" },
+    { text: "Enterprise Population Health 27", url: "#eph27" },
+    { text: "Enterprise Population Health 28", url: "#eph28" },
+    { text: "Enterprise Population Health 29", url: "#eph29" },
+    { text: "Enterprise Population Health 30", url: "#eph30" }
+  ],
+  "Category 6": [
+    { text: "Category6 01", url: "#c61" },
+    { text: "Category6 02", url: "#c62" },
+    { text: "Category6 03", url: "#c63" },
+    { text: "Category6 04", url: "#c64" },
+    { text: "Category6 05", url: "#c65" },
+    { text: "Category6 06", url: "#c66" },
+    { text: "Category6 07", url: "#c67" },
+    { text: "Category6 08", url: "#c68" },
+    { text: "Category6 09", url: "#c69" },
+    { text: "Category6 10", url: "#c610" },
+    { text: "Category6 11", url: "#c611" },
+    { text: "Category6 12", url: "#c612" },
+    { text: "Category6 13", url: "#c613" },
+    { text: "Category6 14", url: "#c614" },
+    { text: "Category6 15", url: "#c615" },
+    { text: "Category6 16", url: "#c616" },
+    { text: "Category6 17", url: "#c617" }
+  ],
+  "Category 7": [
+    { text: "Category Seven 01", url: "#c701" },
+    { text: "Category Seven 02", url: "#c702" },
+    { text: "Category Seven 03", url: "#c703" },
+    { text: "Category Seven 04", url: "#c704" },
+    { text: "Category Seven 05", url: "#c705" },
+    { text: "Category Seven 06", url: "#c706" },
+    { text: "Category Seven 07", url: "#c707" }
+  ],
+  "Category 8": [
+    { text: "Category Eight 01", url: "#c801" },
+    { text: "Category Eight 02", url: "#c802" },
+    { text: "Category Eight 03", url: "#c803" },
+    { text: "Category Eight 04", url: "#c804" },
+    { text: "Category Eight 05", url: "#c805" },
+    { text: "Category Eight 06", url: "#c806" },
+    { text: "Category Eight 07", url: "#c807" }
+  ],
+  "Category 9": [
+    { text: "Category Nine 01", url: "#c901" },
+    { text: "Category Nine 02", url: "#c902" },
+    { text: "Category Nine 03", url: "#c903" },
+    { text: "Category Nine 04", url: "#c904" },
+    { text: "Category Nine 05", url: "#c905" },
+    { text: "Category Nine 06", url: "#c906" },
+    { text: "Category Nine 07", url: "#c907" }
+  ]
+};
+
+function do_menupage(global, document) {
+  var el_mm_page = document.getElementsByClassName("menu-page")[0];
+
+  var mm_page_entries = Object.entries(megamenu);
+
+  var new_div_cat = function(name) {
+    var el_d = document.createElement("H2"); // Create a <div> element
+    el_d.className = "mm-page-category";
+    el_d.innerText = name; // Insert text
+    return el_d;
+  };
+
+  var new_a_link = function(o) {
+    var el_a = document.createElement("A"); // Create a <a> element
+    el_a.className = "mm-page-items-a";
+    el_a.setAttribute("href", o.url);
+    el_a.innerText = o.text; // Insert text
+    return el_a;
+  };
+
+  mm_page_entries.forEach(function(kvp) {
+    var name = kvp[0];
+    var links = kvp[1];
+
+    megamenu[name].div_cols = document.createElement("DIV"); // Create a <div> element
+    megamenu[name].div_cols.className = "mm-page-columns";
+
+    el_mm_page.appendChild(new_div_cat(name));
+
+    links.forEach(function(link) {
+      //add links
+      megamenu[name].div_cols.appendChild(new_a_link(link));
+    });
+
+    el_mm_page.appendChild(megamenu[name].div_cols);
+  });
+}
+if (megamenu) {
+  do_menupage(window, document);
+}
